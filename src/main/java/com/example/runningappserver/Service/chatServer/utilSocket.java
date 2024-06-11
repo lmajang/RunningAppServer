20dd556599af8f50553af169ba2a44630475cfe9
南海十三郎 <lmajang666@outlook.com>
2024-06-12 03:50:48 +0800

    test server1

--- src/main/java/com/example/runningappserver/Service/chatServer/utilSocket.java
+++ src/main/java/com/example/runningappserver/Service/chatServer/utilSocket.java
@@ -1,12 +1,19 @@
 package com.example.runningappserver.Service.chatServer;
 
+import org.json.simple.JSONObject;
+
 public class utilSocket {
     static public void closeConnectSocket(String userId){
         userSocket socket = chatService.getConnectSocketId(userId);
         try {
+            JSONObject connectResult = new JSONObject();
+            connectResult.put("type", "disConnectStatus");
+            socket.getOos().writeObject(connectResult);
+            socket.getOos().flush();
             socket.getSocket().close();
             System.out.println("userId:"+socket.getUserId()+" closeSocket");
             chatService.deleteConnectSocketId(userId);
+
         }catch (Exception e){
             e.printStackTrace();
         }
